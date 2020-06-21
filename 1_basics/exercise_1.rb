8909c43c3e74039b19d246d3337f8cfde84aff28
John Finlay <finlay_john@hotmail.com>
2020-06-21 08:21:26 -0500

    completed basics exercise 1

---
+++ 1_basics/exercise_1.rb
@@ -0,0 +1,8 @@
+# Add two strings together that, when concatenated, 
+# return your first and last name as your full name in one string.
+
+first_name = 'John'
+last_name = 'Finlay'
+full_name = first_name + ' ' + last_name
+
+puts full_name
