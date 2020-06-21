4b7e4a2597a92656b58117eff7a4ca7ff99a1329
John Finlay <finlay_john@hotmail.com>
2020-06-21 09:10:36 -0500

    completed basics exercise 6

---
+++ 1_basics/exercise_6.rb
@@ -0,0 +1,10 @@
+# Write a program that calculates the squares of 3 float numbers of your
+# choosing and outputs the result to the screen.
+
+float_1 = 3.2
+float_2 = 4.6
+float_3 = 3.14159
+
+puts float_1 ** 2
+puts float_2 ** 2
+puts float_3 ** 2
