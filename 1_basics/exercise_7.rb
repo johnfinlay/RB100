942bc79c475d153c02e5b7b388e83d651a5b0e8f
John Finlay <finlay_john@hotmail.com>
2020-06-21 09:15:33 -0500

    completed basics exercise 7

---
+++ 1_basics/exercise_7.rb
@@ -0,0 +1,9 @@
+# What does the following error message tell you?
+#
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+# from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in '<main>'
+
+# My guess is someone created a hash, but instead of closing the hash with
+# a '}', they used a ')'. Typo...
+
+puts 'Nothing to see here, move along...'
