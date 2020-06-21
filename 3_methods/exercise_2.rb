77261f93b4cc8f581094eb2d1ba1e91063589e14
John Finlay <finlay_john@hotmail.com>
2020-06-21 15:45:18 -0500

    completed methods exercise 2

---
+++ 3_methods/exercise_2.rb
@@ -0,0 +1,16 @@
+# What do the following expressions evaluate to?
+#
+# 1. x = 2
+# 2. puts x = 2
+# 3. p name = "Joe"
+# 4. four = "four"
+# 5. print something = "nothing"
+
+
+# 1. 2
+# 2. nil
+# 3. "Joe"
+# 4. "four"
+# 5. nil
+puts "1. 2\n2. nil\n3. \"Joe\"\n4. \"four\"\n5. nil"
+
