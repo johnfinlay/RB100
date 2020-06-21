6984bd66f2cc17a24e674178dab6748a8aab84c0
John Finlay <finlay_john@hotmail.com>
2020-06-21 15:51:45 -0500

    completed methods exercise 3

---
+++ 3_methods/exercise_3.rb
@@ -0,0 +1,9 @@
+# Write a program that includes a method called multiply that takes two
+# arguments and returns the product of the two numbers.
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+puts "3 * 4 is #{multiply(3, 4)}!"
+
