64003c1d96b6f56f0e30b37115f7b058adb1c27c
John Finlay <finlay_john@hotmail.com>
2020-06-23 06:42:35 -0500

    completed loops & iterators exercise 1

---
+++ 5_loops_iterators/exercise_1.rb
@@ -0,0 +1,10 @@
+# What does the each method in the following program return after it is
+# finished executing?
+
+x = [1, 2, 3, 4, 5]
+x.each do |a|
+  a + 1
+end
+
+# When the each method is complete, it returns the original array.
+
