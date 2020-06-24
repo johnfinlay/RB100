c916b22cd6e37174e1b6cf97b6ad7feba700656f
John Finlay <finlay_john@hotmail.com>
2020-06-24 13:32:21 -0500

    completed hashes exercise 7

---
+++ 7_hashes/exercise_7.rb
@@ -0,0 +1,8 @@
+# If you see this error, what do you suspect is the most likely problem?
+
+# NoMethodError: undefined method `keys' for Array
+
+# Most likely you have tried to call the keys method on an Array data type
+# instead of a Hash data type
+
+# Just now saw the multiple choice options, so... B. There is no method called keys for Array objects.
