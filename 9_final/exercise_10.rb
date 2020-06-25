e81e302f0b3d825ff1d653c1dc937a4118962b90
John Finlay <finlay_john@hotmail.com>
2020-06-25 18:08:23 -0500

    completed final exercise 10

---
+++ 9_final/exercise_10.rb
@@ -0,0 +1,10 @@
+# Can hash values be arrays? Can you have an array of hashes? (give examples)
+
+# Hash values AND keys can be arrays.
+# Yes, you can have an array of hashes
+
+hash_with_array = {array: [1, 2, 3]}
+p hash_with_array
+
+array_with_hash = [{one: 1, two: 2}, {three: 3}]
+p array_with_hash
