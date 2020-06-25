d8a649b9e1d17878dac2aef5e2b20d4857179c47
John Finlay <finlay_john@hotmail.com>
2020-06-25 17:37:25 -0500

    completed More Stuff exercise 3

---
+++ 8_more_stuff/exercise_3.rb
@@ -0,0 +1,6 @@
+# What is exception handling and what problem does it solve?
+
+# Exception handing is catching and reporting errors inside of your code.
+# It allows a graceful way to deal with known errors, allows the program
+# to continue execution, and allows a way to effectively communicate
+# with the user regarding "expected" errors(data validation)
