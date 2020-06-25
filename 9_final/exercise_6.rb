8c2efd6fda98dd2a6e9d5d0e7f9577630704a3cb
John Finlay <finlay_john@hotmail.com>
2020-06-25 17:56:31 -0500

    completed final exercise 6

---
+++ 9_final/exercise_6.rb
@@ -0,0 +1,10 @@
+# Get rid of duplicates without specifically removing any one value.
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+arr.push(11)
+arr.insert(0, 0)
+arr.pop
+arr.push(3)
+arr.uniq!
+p arr
